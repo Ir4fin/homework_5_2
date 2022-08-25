ee5ddab69fd9d94f837a652e7831772c528857d0
Ir4fin <messu91@gmail.com>
2022-08-25 21:22:25 +0500

    added compare plans test

---
+++ src/test/java/github/comparePlans.java
@@ -0,0 +1,46 @@
+package github;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.text;
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+
+public class comparePlans {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+    @Test void pageShouldLoads (){
+        //open github.com
+        open("https://github.com");
+        //bring mouse over "pricing"
+        $(byText("Pricing")).hover();
+        //click on "Compare plans"
+        $(byText("Compare plans")).click();
+        //check if opened page is right
+        $("h1.h1").shouldHave(text("Compare features"));
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
