c4c760732abce4ef1891d1819148a3dce53d9602
Ir4fin <messu91@gmail.com>
2022-08-25 21:56:02 +0500

    added drag & drop test

--- src/test/java/herokuapp/dragAndDrop.java
+++ src/test/java/herokuapp/dragAndDrop.java
@@ -1,5 +1,9 @@
 package herokuapp;
 
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,14 +11,20 @@ import org.junit.jupiter.api.Test;
 public class dragAndDrop {
 
     @BeforeAll
-    static void configure() {
+        static void configure() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://the-internet.herokuapp.com/"
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
     }
 
     @Test
-    void dragRectangleAToRectangleB(){
-
+        void dragRectangleAToRectangleB(){
+        //open page drag and drop
+        open("/drag_and_drop");
+        //drag rectangle A to rectangle B and vice versa
+        $("#column-a").dragAndDropTo("#column-b");
+        //check for correct changes
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 
     }
 
